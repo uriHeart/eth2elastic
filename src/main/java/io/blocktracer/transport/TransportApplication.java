c3d392d58b33cc0d45eeccce29449c9db8480ed3
uriHeart <karismmm@gmail.com>
2018-09-13 14:06:17 +0900

    test

--- src/main/java/io/blocktracer/transport/TransportApplication.java
+++ src/main/java/io/blocktracer/transport/TransportApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransportApplication {
 
+    //$$
     public static void main(String[] args) {
         SpringApplication.run(TransportApplication.class, args);
     }
